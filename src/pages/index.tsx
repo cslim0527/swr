4a8c4c9165019e45c1951309794d4bd8ecf27285
limchansoo <cslim0527@gmail.com>
2022-10-15 06:45:01 +0900

    useSWR hook을 사용한 데이터 패칭

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import CreatePost from "@components/CreatePost";
 import PostCard from "@components/PostCard";
-import { useEffect, useState } from "react";
-import axios from "axios";
 import Loader from "@components/Loader";
+import useSWR from "swr";
+import axios from "axios";
 import { IPost } from "@libs/types";
 
 export default function Home() {
-  const [posts, setPosts] = useState<IPost[]>(null);
+  /* const [posts, setPosts] = useState<IPost[]>(null);
 
   const getPosts = async () => {
     const { data } = await axios.get("/posts?_sort=createdAt&_order=desc");
@@ -15,15 +15,20 @@ export default function Home() {
 
   useEffect(() => {
     getPosts();
-  }, []);
+  }, []);*/
+
+  const { data: posts, error } = useSWR<IPost[]>(
+    "/posts?_sort=createdAt&_order=desc"
+  );
 
   return (
     <div>
       <h4>useSWR Hook ⛳</h4>
-      <CreatePost setPosts={setPosts} />
+      {/*<CreatePost setPosts={setPosts} />*/}
 
       <h4>Posts</h4>
 
+      {error && <p>Something is wrong!</p>}
       {!posts && <Loader />}
       {posts?.map((post) => (
         <PostCard key={post.id} data={post} />
